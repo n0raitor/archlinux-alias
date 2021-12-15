daf4e7c0ba48b7ed0b51f91968e34f7ac0f5153f
Norman Schmidt <mail@norman-schmidt.com>
2021-12-15 12:23:33 +0100

    INIT

---
+++ alias.sh
@@ -0,0 +1,41 @@
+# Main ALIAS
+alias ls="ls --color=auto"
+alias l="ls --color=auto -lA --time-style long-iso"
+alias ll="ls --color=auto -la --time-style long-iso"
+alias cd..="cd .."
+alias ..="cd .."
+alias ...="cd ../../"
+alias ....="cd ../../../"
+alias .....="cd ../../../../"
+alias ff="find / -name"
+alias f="find . -name"
+alias grep="grep --color=auto"
+alias egrep="egrep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias ip="alias ip='ip -c'"
+alias pacman="pacman --color auto"
+alias pactree="pactree --color"
+alias vdir="vdir --color=auto"
+alias watch="watch --color"
+alias man="color function"
+alias yay="sudo -u norman yay"
+alias browser="midori &"
+
+# Kali Undercover Mode
+alias panic="kali-undercover"
+alias ku="kali-undercover"
+alias qwer="kali-undercover"
+
+# CUSTOM Additional ALIAS
+alias pgadmin="sudo /usr/pgadmin4/bin/pgadmin4"
+alias haskel="ghci"
+alias cyberghost="sudo cyberghostvpn --traffic sudo cyberghostvpn --traffic --connect --country-code"
+alias cyberghostlist="cyberghostvpn --traffic --country-code"
+alias spotifys="spotify --force-device-scale-factor=2.0 %U"
+alias vi="vim"
+
+# German Man Page
+alias man="LANG=de_DE.UTF-8 man"
+
+# Integrate Most in Man
+export PAGER=most
